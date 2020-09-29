ec4d26d8bf88def7b9db34163cb5f9d2ce5389f4
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-09-29 16:50:31 -0400

    adds ability to search one level deep from the menu.

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,16 +11,40 @@ class Cli
         puts ""
         puts "*************************"
         puts ""
-        puts "To see a list of all spells, type 'spells'."
-        puts ""
-        puts "To exit the program, type 'exit'."
-        puts ""
-        Api.import_spells
-        @input = gets.strip.downcase
-        if @input.downcase == "spells"
-            Spell.list_spells
-        elsif @input.downcase == "exit"
-            exit
+        @input = "lorum ipsum"
+        until @input.downcase == "spells" || @input == "exit"
+            puts "To see a list of all spells, type 'spells'."
+            puts ""
+            puts "To exit the program, type 'exit'."
+            puts ""
+            Api.import_spells
+            binding.pry
+            @input = gets.strip.downcase
+            if @input == "spells"
+                Spell.list_spells
+                puts ""
+                puts "Thanks for waiting through that."
+                puts ""
+                puts "Now, What spell do you want to know about?"
+                @input = "lorum ipsum"
+                #search metadata for the name
+                until Api.metadata.has_value?(@input)
+                    puts "Type a spell name to learn more about the spell."
+                    @input = gets.strip
+                    if Api.metadata.has_value?(@input)
+                        spell = Spell.find_or_create_by_name(@input)
+                        puts "What would you like to know about this spell?"
+                        puts "To know what it does, type 'description'."
+                        puts "To know its range and duration, type 'casting'."
+                        puts "to know its level and if it has any requirements, type 'requirements'."
+                        #need to code the next level of search
+                    else
+                        puts "Please check the spelling of your spell."
+                    end
+                end
+            elsif @input.downcase == "exit"
+                exit
+            end
         end
     end
 end
